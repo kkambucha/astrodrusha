b6795036f1a1b3533154c2f3404c0fe790c4b22f
Vadim Zakovryashin <v.zakovryashin@corp.mail.ru>
2017-05-23 16:17:08 +0300

    Init project

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import ThemeProvider from 'react-toolbox/lib/ThemeProvider' ;
+import theme from './assets/react-toolbox/theme.js';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import './assets/react-toolbox/theme.css';
+
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>, document.getElementById('root'));
+registerServiceWorker();
